f35ff4f7eeb382dbac5eb4a89114de3819ad0dfe
Anfacibry <jogos.fabriciocintra@gmail.com>
2022-11-16 09:38:41 -0300

    Criando pasta com dados persistentes

---
+++ lib/data/repositorio.dart
@@ -0,0 +1,132 @@
+import 'package:get/get.dart';
+
+class Repositorio extends GetxController {
+  List<Map<String, String>> empresa = [
+    {
+      "chave": "1",
+      "descricao": "MEDICICOR MATRIZ",
+    },
+    {
+      "chave": "2",
+      "descricao": "MEDICICOR GALPÃO",
+    },
+    {
+      "chave": "3",
+      "descricao": "MEDICICOR RECIFE",
+    },
+    {
+      "chave": "4",
+      "descricao": "MEDICICOR ARACAJU",
+    },
+    {
+      "chave": "5",
+      "descricao": "MEDICICOR MACEIO",
+    },
+    {
+      "chave": "6",
+      "descricao": "ELEVEMED",
+    },
+    {
+      "chave": "7",
+      "descricao": "ELEVEMED MG",
+    },
+    {
+      "chave": "8",
+      "descricao": "MEDICICOR NATAL",
+    },
+    {
+      "chave": "9",
+      "descricao": "MEDICICOR FORTALEZA",
+    },
+    {
+      "chave": "10",
+      "descricao": "MEDICICOR JOÃO PESSOA",
+    },
+  ].obs;
+
+  List<Map<String, String>> parceiro = [
+    {
+      "chave": "2",
+      "descricao": "SERVIÇO DE ASSIT. MEDICA E URGENCIA SAMUR",
+    },
+    {
+      "chave": "3",
+      "descricao": "INSTITUTO RODOLFO NEIROTTI DE GERENC. E PESQ. LTDA",
+    },
+    {
+      "chave": "6",
+      "descricao": "FUNDAÇÃO BAHIANA DE CARDIOLOGIA",
+    },
+    {
+      "chave": "7",
+      "descricao": "CARDIO PULMONAR DA BAHIA SA",
+    },
+    {
+      "chave": "8",
+      "descricao": "FAPEX - FUNDAÇÃO DE APOIO A PESQUISA E EXTENSÃO",
+    },
+    {
+      "chave": "9",
+      "descricao": "REAL SOCIEDADE PORTUGUESA BENEF. 16 DE SETEMBRO",
+    },
+    {
+      "chave": "10",
+      "descricao": "HOSPITAL E MATERNIDADE PRIMAVERA",
+    },
+    {
+      "chave": "11",
+      "descricao": "HOSPITAL SÃO LUCAS MEDICO E HOSPITALAR LTDA",
+    },
+    {
+      "chave": "13",
+      "descricao": "HOSPITAL DA BAHIA LTDA",
+    },
+    {
+      "chave": "14",
+      "descricao": "REAL SOCIEDADE ESPANHOLA DE BENEFICENCIA",
+    },
+    {
+      "chave": "15",
+      "descricao": "HOSPITAL PROHOPE LTDA",
+    },
+  ].obs;
+
+  List<Map<String, String>> procedimento = [
+    {
+      "chave": "1",
+      "descricao": "CONSULTA",
+    },
+    {
+      "chave": "2",
+      "descricao": "EXAME DE SANGUE",
+    },
+    {
+      "chave": "3",
+      "descricao": "TOMOGRAFIA",
+    },
+    {
+      "chave": "4",
+      "descricao": "CONSULTA DE DÉBITO",
+    },
+    {
+      "chave": "5",
+      "descricao": "FINANÇAS",
+    },
+    {
+      "chave": "6",
+      "descricao": "CADERNO",
+    },
+    {
+      "chave": "7",
+      "descricao": "FLUTTER",
+    },
+    {
+      "chave": "8",
+      "descricao": "CARAMELO",
+    },
+    {
+      "chave": "9",
+      "descricao": "FABRICIO CINTRA",
+    },
+  ].obs;
+}

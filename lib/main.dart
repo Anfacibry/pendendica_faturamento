b2183d225f1500d4f590c414fc2318b27fb02b1f
Anfacibry <jogos.fabriciocintra@gmail.com>
2022-11-16 09:43:33 -0300

    Jogando as atualizações no main principal

--- lib/main.dart
+++ lib/main.dart
@@ -15,11 +15,17 @@ class PendenciaFaturamento extends StatelessWidget {
     return GetMaterialApp(
       theme: ThemeData(
         textTheme: const TextTheme(
-            headline3: TextStyle(
-          fontSize: 18,
-          color: Colors.white,
-          fontWeight: FontWeight.w600,
-        )),
+          headline3: TextStyle(
+            fontSize: 18,
+            color: Colors.white,
+            fontWeight: FontWeight.w600,
+          ),
+          bodyText2: TextStyle(
+            fontSize: 20,
+            color: Colors.blue,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
       ),
       debugShowCheckedModeBanner: false,
       home: const TelaPendenciaFaturamento(),
